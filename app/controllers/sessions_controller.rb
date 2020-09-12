719ed3f7005cca222b340d1ea2eebc5663b37938
watanabechi <watanabechinami@gmail.com>
2020-09-12 05:11:00 +0000

    Finish editing-users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = "認証に失敗しました。"
       render :new
@@ -20,4 +20,4 @@ class SessionsController < ApplicationController
       flash[session] = "ロウアウトしました。"
       redirect_to root_url
     end
-end 
+end
